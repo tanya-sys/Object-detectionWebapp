39c0e6a11a1071f77871f29429fed099f8376ffd
tanya-sys <95346729+tanya-sys@users.noreply.github.com>
2021-12-01 17:06:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+img="";
+Status="";
+objectdetector="";
+objects=[];
+function preload(){
+    img=loadImage('dog_cat.jpg');
+};
+function setup(){
+    canvas=createCanvas(350,350);
+    canvas.center();
+    objectdetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : detecting object";
+};
+function draw(){
+    image(img,0,0,350,350);
+    
+  //  stroke("FF0000");
+  if(Status != ""){
+      for(i=0; i<objects.length; i++){
+          document.getElementById("status").innerHTML="Status : object detected";
+          fill("#FF0000");
+          percent=floor(objects[i].confidence *100);
+          text(objects[i].label + " "+percent+"%",objects[i].x,objects[i].y);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+      }
+  }
+};
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status=true;
+    objectdetector.detect(img,gotResult);
+   
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results);
+    objects=results;
+}
